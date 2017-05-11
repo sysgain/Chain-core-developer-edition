62ffabb301088443d26583dabf2c37be4bde4119
PradeepTS <pradeepts@sysgain.com>
2017-05-11 09:57:38 +0530

    Update signer-script.sh

--- scripts/signer-script.sh
+++ scripts/signer-script.sh
@@ -1,38 +1,49 @@
 #script to generate signin key and netwowrk token
 #Usage : sh signer-script.sh <ip/dnsname of the signer VM>
+
 #!/bin/bash
 
-#host name of the signer vm
-host=$1
-port="1999"
-
-# install the required packages
+# install prerequisites 
 apt-get update && apt-get install -y libssl-dev libffi-dev python-dev build-essential && apt-get install -y nodejs-legacy && apt-get install -y npm
 
-npm install -g azure-cli
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | \
+     sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893
+sudo apt-get install apt-transport-https
+sudo apt-get update && sudo apt-get install azure-cli
+sudo apt-get update && sudo apt-get install azure-cli
 
-# pull chaincore docker image
-docker pull chaincore/developer:latest
+generatornodeip=$1
+serviceprincipal=$2
+secretkey=$3
+tenatid=$4
+subscriptionid=$5
+keyvaultname=$6
+#signerclienttoken=$7
+clienttokenname=$7 
+az login --service-principal -u $serviceprincipal -p $secretkey --tenant $tenatid
+az account set -s $subscriptionid
+
+generatornetworktoken=`az keyvault secret show --name networkToken --vault-name $keyvaultname | grep "value" | cut -d "\"" -f4`
+blockchainid=`az keyvault secret show --name chaincoreid --vault-name $keyvaultname | grep "value" | cut -d "\"" -f4`
+
 
 # run chaincore docker image
-docker run -itd -p 1999:1999 chaincore/developer:latest
+docker run -d -p 1999:1999 chaincore/developer:latest
+sleep 30
+containerId=`docker ps | cut -d " " -f1 | sed 1d`
 
-containerId=`docker ps | cut -d " " -f1 |sed 1d`
-# grab the client token of signer
-export clientToken=`docker logs $containerId | grep client | cut -c22-`
-docker exec -itd $containerId /bin/sh
+#generator client access token / public key
+cclength=`echo $clienttokenname | wc -c`
+cclength1=`expr $cclength + 1`
+docker exec  $containerId /usr/bin/chain/cored
+signerctoken=`docker exec  $containerId /usr/bin/chain/corectl create-token $clienttokenname | cut -c1-71`
 
-#signer public key
-docker exec -itd $containerId /usr/bin/chain/corectl reset
-export signerpubkey=`docker exec -it $containerId /usr/bin/chain/corectl create-block-keypair`
-export netToken=`docker exec -it $containerId /usr/bin/chain/corectl create-token -net foo`
-networktoken=`echo $netToken | cut -c1-68`
+signerctoken1=`echo $signerctoken | cut -c$cclength1-`
+docker exec  $containerId /usr/bin/chain/corectl config -t $generatornetworktoken -k $signerctoken1 $blockchainid http://$generatornodeip:1999
+#generator blockchain_id
 
-signerccurl="https://$networktoken@$host:$port"
 
-#connecting to keyvault and sending values to key Vault
+sudo docker restart $containerId
 
-azure login -u asebastian@sysgaininc.onmicrosoft.com -p Ashwinse2793
-azure keyvault secret set --secret-name secretname1 --vault-name Chain-e5yge-keybunch --value $signerpubkey
-azure keyvault secret set --secret-name secretname2 --vault-name Chain-e5yge-keybunch --value $signerccurl
-azure keyvault secret set --secret-name secretname3 --vault-name Chain-e5yge-keybunch --value $clientToken
+az keyvault secret set --name signerclienttoken1 --vault-name $keyvaultname --value $signerctoken
