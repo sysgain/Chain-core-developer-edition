136c8e09ca34226856cf399a08db23d0a17911e7
PradeepTS <pradeepts@sysgain.com>
2017-05-31 12:43:41 +0530

    Update participant-script.sh

--- scripts/participant-script.sh
+++ scripts/participant-script.sh
@@ -22,6 +22,13 @@ echo "===========================================Installing Azure CLI 2.0=======
 sudo apt-get install azure-cli
 sudo apt-get update 
 sudo apt-get install azure-cli
+echo "===========================================Installing Docker=================================================="
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+sudo systemctl enable docker
 echo "===========================================Logging into azure account==========================================="
 az login --service-principal -u $serviceprincipal -p $secretkey --tenant $tenatid
 az account set -s $subscriptionid
